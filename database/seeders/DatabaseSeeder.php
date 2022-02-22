e3eb2b965f7230811aca1044e057d8df27453478
linlaz <89636741+linlaz@users.noreply.github.com>
2022-02-22 13:32:38 +0700

    1%

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,6 +17,78 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $admin = Role::create([
+            'name' => 'admin'
+        ]);
+        Role::create([
+            'name' => 'user'
+        ]);
+        Role::create([
+            'name' => 'kestary'
+        ]);
+        Role::create([
+            'name' => 'event'
+        ]);
+        Role::create([
+            'name' => 'kompetisi'
+        ]);
+        $showrole = Permission::create([
+            'name' => 'show-role'
+        ]);
+        $addrole = Permission::create([
+            'name' => 'add-role'
+        ]);
+        $editrole = Permission::create([
+            'name' => 'edit-role'
+        ]);
+        $deleterole = Permission::create([
+            'name' => 'delete-role'
+        ]);
+        $addpermission = Permission::create([
+            'name' => 'add-permission'
+        ]);
+        $editpermission = Permission::create([
+            'name' => 'edit-permission'
+        ]);
+        $deletepermission = Permission::create([
+            'name' => 'delete-permission'
+        ]);
+        $edituser = Permission::create([
+            'name' => 'edit-user'
+        ]);
+        $giveroleuser = Permission::create([
+            'name' => 'giverole-user'
+        ]);
+        $givepermissionuser = Permission::create([
+            'name' => 'givepermission-user'
+        ]);
+        $blokuser = Permission::create([
+            'name' => 'blok-user'
+        ]);
+        $deleteuser = Permission::create([
+            'name' => 'delete-user'
+        ]);
+        $addevent = Permission::create([
+            'name' => 'add-event'
+        ]);
+        $editevent = Permission::create([
+            'name' => 'edit-event'
+        ]);
+        $deleteevent = Permission::create([
+            'name' => 'delete-event'
+        ]);
+        $admin->givePermissionTo(['add-role', 'edit-role', 'delete-role', 'add-permission', 'edit-permission', 'delete-permission']);
+
+        // ModelsCategory::create([
+        //     'name_category' => 'Webinar',
+        //     'slug_category' => 'Webinar',
+        //     'active' => "1"
+        // ]);
+        $user = User::create([
+            'name' => 'lintang lazuardi',
+            'password' => bcrypt('linlaz11'),
+            'email' => 'lazuardilintang@apps.ipb.ac.id',
+        ]);
+        $user->assignRole('admin');
     }
 }
