322e33ef996d53078d2490d569a4c1dc47b398f6
linlaz <89636741+linlaz@users.noreply.github.com>
2022-05-25 19:56:14 +0700

    delete dompdf

--- app/Http/Controllers/RegistrantCompetitionController.php
+++ app/Http/Controllers/RegistrantCompetitionController.php
@@ -28,18 +28,4 @@ class RegistrantCompetitionController extends Controller
             'name_tim' => $namereg,
         ]);
     }
-    public function indexdetailregistrantcompetitionbycompetitiondashboardpdf($slug, $namereg)
-    {
-        // $pdf = PDF::loadView('dashboard.registrantcompetition.detailregistrantcompetition',[
-        //     'name_competition' => $slug,
-        //     'name_tim' => $namereg,
-        // ])->setOptions(['defaultFont' => 'sans-serif']);
-
-        // return $pdf->download('user.pdf');
-        // return view('dashboard.registrantcompetition.detailregistrantcompetition',[
-        //     'name_competition' => $slug,
-        //     'name_tim' => $namereg,
-        // ]);
-        return redirect('/');
-    }
 }
